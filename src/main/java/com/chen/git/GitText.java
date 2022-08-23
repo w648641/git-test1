b3bff0d4349d318b322575b31fbc9418d1960337
chen <648641699@qq.com>
2022-08-23 22:15:01 +0800

    second commit

--- src/main/java/com/chen/git/GitText.java
+++ src/main/java/com/chen/git/GitText.java
@@ -3,5 +3,6 @@ package com.chen.git;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
